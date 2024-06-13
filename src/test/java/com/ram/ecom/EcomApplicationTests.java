db3282de4c62ea33eff7cc3a13d536271017ec40
ankit <ankitnarale1000@gmail.com>
2024-06-13 12:47:27 +0530

    Project created

---
+++ src/test/java/com/ram/ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
